5bf5f34b2ed1c23c57e33eb0d8e870cbbed8c411
OYEBAMIJI <HARYOURBHAMIE@AOL.COM>
2020-04-12 13:49:38 +0100

    my javaScript uoloaded

---
+++ index.js
@@ -0,0 +1,2 @@
+// this is my first javaScirpt code!
+console.log('hello world');
---
+++ index1.js
@@ -0,0 +1,24 @@
+
+let name = 'Ayobami';
+let age = 30;
+let isApproved = false;
+let firstName = undefined;
+let selectedColor = null;
+undefined
+let name = 'Ayobami'
+console.log(name);
+
+// variables cannot be a reserved keyword
+// should be meaningful
+//cannot start with a number (1name)
+// cannot contain a space or hyphen (-)
+//are case sensitve
+
+let firstName ='Ayobami';
+let otherName = 'Mathew';
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+// if the value of your variable would change in real time use 'let' declaration, but if it should be constant, use 'const' to declear the varaible
