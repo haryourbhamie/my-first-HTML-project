2d8188380e4e73eaae93d041abcf4a3d1c005ffb
OYEBAMIJI <HARYOURBHAMIE@AOL.COM>
2020-04-12 17:20:50 +0100

    an ARRAY in an object

--- index2.js
+++ index2.js
@@ -4,4 +4,12 @@ let person = {
     age: 30,
 };
 
+// dot Notation
+person.name = 'Mathew';
+
+// Bracket Notation is used when the property is to be determined during runtime
+let selection = 'name';
+person[selection] = 'Oye';
+
 console.log(person)
+
---
+++ index3.js
@@ -0,0 +1,6 @@
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 'pink';
+console.log(selectedColors);
+//you can check the property of object using Dot Notation
+console.log(selectedColors.length);
+// an ARRAY is a data structure that is used to represent a list of items
