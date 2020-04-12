163dda84100b65737602a50d8554b76e47255d7a
OYEBAMIJI <HARYOURBHAMIE@AOL.COM>
2020-04-12 19:05:35 +0100

    performing a task and calculating a value types of function

--- index4.js
+++ index4.js
@@ -1,6 +1,12 @@
 
+//performing a task
 function greet(name, lastName) {
     console.log('hello ' + name + ' ' + lastName );
 }
 
-greet('Ayobami', 'Mathew');
+greet('Ayobami', 'Mathew')
+//calculating a value
+function square(number){
+    return number*number
+}
+console.log(square(4));
